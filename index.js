418877721f99d85a29b54cf6e81f186e1488aafe
hudhamalick <noorulcse@gmail.com>
2022-03-19 02:43:33 +0530

    update

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 
 dotenv.config();
-console.log(process.env.MONGO_URL)
+//console.log(process.env.MONGO_URL)
 const app = express();
 
-const PORT = 4000;
+const PORT = process.env.PORT;
 
 const movies = [
   {
